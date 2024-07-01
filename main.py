90856aa56f08d8ddeef10d3c3cd256b6e6889b64
Alireza Karimi <Alireza.Karimi.Programmer@gmail.com>
2024-07-01 20:34:57 +0330

    complete the project

--- main.py
+++ main.py
@@ -23,16 +23,25 @@ while cap.isOpened():
     for contour in contours:
         
         # making edge for shape:
-        approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
+        approx = cv2.approxPolyDP(contour, 0.1 * cv2.arcLength(contour, True), True)
         
         # Check if edges equal the 4:
         if len(approx) == 4:
     
             # finding the edge of Rectange:
-            x,y,w,h = cv2.boundingRect(contour)
+            x, y, w, h = cv2.boundingRect(approx)
             
-            # Draw The rectangel on frame:
-            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
+            if (100,100) <= (w, h) <= (900, 900):
+                
+                # draw The rectangel on frame:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                
+                # point to the center of rectangle
+                cv2.circle(frame, (int(x + w / 2), int(y + h / 2)), 1, (0,0,255), 3)
+                
+                # put text up above the shape founded:
+                cv2.putText(frame, str((w, h)), (x, y - 10),
+                            cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255),2)
     
     # Shows the Video:
     cv2.imshow("Founded", frame)
